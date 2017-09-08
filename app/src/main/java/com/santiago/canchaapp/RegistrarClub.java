ae89c54ff824a956830b7bffe8868c31e9ce642d
santileira <santiagoleira10@gmail.com>
2017-09-08 18:43:14 -0300

    empty project

---
+++ app/src/main/java/com/santiago/canchaapp/RegistrarClub.java
@@ -0,0 +1,13 @@
+package com.santiago.canchaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrarClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_club);
+    }
+}
