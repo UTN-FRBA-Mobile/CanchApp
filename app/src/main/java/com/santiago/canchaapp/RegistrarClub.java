1fcef478cff2884e81f78af3f030d5eb488eada7
santileira <santiagoleira10@gmail.com>
2017-09-08 20:16:37 -0300

    agrego pantalla registrar club, dependencias butter knife

--- app/src/main/java/com/santiago/canchaapp/RegistrarClub.java
+++ app/src/main/java/com/santiago/canchaapp/RegistrarClub.java
@@ -2,12 +2,35 @@ package com.santiago.canchaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class RegistrarClub extends AppCompatActivity {
+
+    @BindView(R.id.rangoHorario)
+    public CrystalRangeSeekbar rangoHorario;
+    @BindView(R.id.txtValorMinimo)
+    public TextView valorMinimo;
+    @BindView(R.id.txtValorMaximo)
+    public TextView valorMaximo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_club);
+        ButterKnife.bind(this);
+        rangoHorario.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number minValue, Number maxValue) {
+                valorMinimo.setText(String.valueOf(minValue));
+                valorMaximo.setText(String.valueOf(maxValue));
+            }
+        });
     }
 }
