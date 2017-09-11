ae9779d0aae4baa3b93da8b7ceb9aff76aba36a1
santileira <santiagoleira10@gmail.com>
2017-09-10 23:27:22 -0300

    agrego butterknife en cargar fotos

--- app/src/main/java/com/santiago/canchaapp/CargarFotosCancha.java
+++ app/src/main/java/com/santiago/canchaapp/CargarFotosCancha.java
@@ -6,18 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CargarFotosCancha extends AppCompatActivity {
     private ImageButton btn_cargarFotosCancha;
 
+    @BindView(R.id.btnImagen01)
+    public ImageButton btnCargarFotosCancha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cargar_fotos_cancha);
-
-        btn_cargarFotosCancha = (ImageButton) findViewById(R.id.btnImagen01);
-        btn_cargarFotosCancha.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
+        btnCargarFotosCancha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Esto si funciona",Toast.LENGTH_LONG).show();
