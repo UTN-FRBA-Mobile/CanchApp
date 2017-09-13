85eddf5be594437ce8df623d319ca6dcd6077f0e
santileira <santiagoleira10@gmail.com>
2017-09-13 13:36:55 -0300

    autenticacion con firebase

--- app/src/main/java/com/santiago/canchaapp/LoginActivity.java
+++ app/src/main/java/com/santiago/canchaapp/LoginActivity.java
@@ -1,11 +1,8 @@
 package com.santiago.canchaapp;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,24 +13,22 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-
-import java.io.IOException;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private GoogleApiClient mGoogleApiClient;
-    private SignInButton mGoogleLoginButton;
+    @BindView(R.id.btnLogin)
+    public SignInButton mGoogleLoginButton;
     private static final int RC_SIGN_IN = 9001;
     private static final String TAG = LoginActivity.class.getSimpleName();
 
@@ -41,7 +36,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mGoogleLoginButton = (SignInButton) findViewById(R.id.login_with_google);
+        ButterKnife.bind(this);
+
         mGoogleLoginButton.setOnClickListener(LoginActivity.this);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -52,16 +48,22 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
         mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
     }
 
     @Override
     public void onClick(View v) {
-        signIn();
+        int i = v.getId();
+        switch (i) {
+            case R.id.btnLogin:
+                signIn();
+                break;
+        }
     }
 
-    public void signIn(){
-        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(intent, RC_SIGN_IN);
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
@@ -76,26 +78,26 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             else
                 Toast.makeText(LoginActivity.this, "Ha ocurrido un error", Toast.LENGTH_LONG).show();
         }
+        else
+            Toast.makeText(LoginActivity.this, "Ha ocurrido un error", Toast.LENGTH_LONG).show();
     }
 
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        Toast.makeText(LoginActivity.this,""+credential.getProvider(),Toast.LENGTH_LONG).show();
+        Toast.makeText(LoginActivity.this, "firebaseAuthWithGoogle", Toast.LENGTH_LONG).show();
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful())
-                            Toast.makeText(LoginActivity.this,"Something went well",Toast.LENGTH_LONG).show();
+                            Toast.makeText(LoginActivity.this, "Algo ha ocurrido bien", Toast.LENGTH_LONG).show();
                         else
-                            Toast.makeText(LoginActivity.this,"Something went wrong",Toast.LENGTH_LONG).show();
+                            Toast.makeText(LoginActivity.this, "Algo ha ocurrido mal", Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-    }
-
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
 }
