f5b6793cd2afc9b3771824633b8184be4cd04b74
Felipe <felipecalvo239@gmail.com>
2017-09-25 18:13:22 -0300

    agrego floating action button

--- app/src/main/java/com/santiago/canchaapp/app/fragment/RegistrarCanchasFragment.java
+++ app/src/main/java/com/santiago/canchaapp/app/fragment/RegistrarCanchasFragment.java
@@ -1,6 +1,34 @@
 package com.santiago.canchaapp.app.fragment;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
+import com.santiago.canchaapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegistrarCanchasFragment extends Fragment {
+
+    @BindView(R.id.fab)
+    public FloatingActionButton fab;
+
+    public static RegistrarCanchasFragment nuevaInstancia() {
+        return new RegistrarCanchasFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_registrar_canchas, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
 }
