512b66a5c54f6a2b76be022464dbfae8002011c4
Felipe <felipecalvo239@gmail.com>
2017-09-24 21:13:05 -0300

    comienzo pantalla para registrar canchas

--- app/src/main/java/com/santiago/canchaapp/app/fragment/MapClubFragment.java
+++ app/src/main/java/com/santiago/canchaapp/app/fragment/MapClubFragment.java
@@ -1,22 +1,34 @@
 package com.santiago.canchaapp.app.fragment;
 
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.santiago.canchaapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.santiago.canchaapp.app.otros.FragmentTags.REGISTRAR_CANCHAS;
 
 public class MapClubFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    @BindView(R.id.btnContinuar)
+    public Button continuar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +36,25 @@ public class MapClubFragment extends Fragment implements OnMapReadyCallback {
         View view = inflater.inflate(R.layout.fragment_map_club, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_club);
         mapFragment.getMapAsync(this);
+
+        ButterKnife.bind(this, view);
+
+        continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirFragment();
+            }
+        });
         return view;
     }
 
-
+    private void abrirFragment() {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, new RegistrarCanchasFragment(), REGISTRAR_CANCHAS.toString())
+                .addToBackStack(null)
+                .commit();
+    }
 
     /**
      * Manipulates the map once available.
@@ -41,7 +68,8 @@ public class MapClubFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        MapStyleOptions style = MapStyleOptions.loadRawResourceStyle(getContext(), R.raw.style_json);
+        mMap.setMapStyle(style);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
---
+++ app/src/main/java/com/santiago/canchaapp/app/fragment/RegistrarCanchasFragment.java
@@ -0,0 +1,6 @@
+package com.santiago.canchaapp.app.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class RegistrarCanchasFragment extends Fragment {
+}
