3751f8176e57fde09dba789168ad3be0c7d8ae33
Braian Halblaub <braianhal@gmail.com>
2017-10-01 20:26:48 -0300

    Validaciones en registrar cancha

--- app/src/main/java/com/santiago/canchaapp/app/fragment/RegistrarClubFragment.java
+++ app/src/main/java/com/santiago/canchaapp/app/fragment/RegistrarClubFragment.java
@@ -1,27 +1,39 @@
 package com.santiago.canchaapp.app.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
 import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
 import com.santiago.canchaapp.R;
 
-import static com.santiago.canchaapp.app.otros.FragmentTags.MAP_CLUB;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static com.santiago.canchaapp.app.otros.FragmentTags.MAP_CLUB;
+import static com.santiago.canchaapp.app.otros.TextUtils.esUnEmail;
+import static com.santiago.canchaapp.app.otros.TextUtils.estaVacio;
 
 
 
 public class RegistrarClubFragment extends Fragment{
 
+    @BindView(R.id.txtNombre)
+    public EditText txtNombre;
+    @BindView(R.id.txtTelefono)
+    public EditText txtTelefono;
+    @BindView(R.id.txtEmail)
+    public EditText txtEmail;
     @BindView(R.id.rangoHorario)
     public CrystalRangeSeekbar rangoHorario;
     @BindView(R.id.txtValorMinimo)
@@ -50,10 +62,23 @@ public class RegistrarClubFragment extends Fragment{
         continuar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abrirFragmentSiguiente();
+                if (validarCampos(view.getContext())) {
+                    abrirFragmentSiguiente();
+                }
             }
         });
         return view;
+    }
+
+    private boolean validarCampos(Context context) {
+        if (estaVacio(nombre()) || estaVacio(telefono()) || estaVacio(email())) {
+            Toast.makeText(context, R.string.txtCompletarTodosLosCampos, LENGTH_SHORT).show();
+            return false;
+        } else if (!esUnEmail(email())) {
+            Toast.makeText(context, R.string.txtEmailIncorrecto, LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
     private void abrirFragmentSiguiente() {
@@ -62,6 +87,20 @@ public class RegistrarClubFragment extends Fragment{
                 .replace(R.id.content_frame, mapClubFragment, MAP_CLUB.toString())
                 .addToBackStack(null)
                 .commit();
+    }
+
+    // Utils
+
+    private String nombre() {
+        return txtNombre.getText().toString();
+    }
+
+    private String telefono() {
+        return txtTelefono.getText().toString();
+    }
+
+    private String email() {
+        return txtEmail.getText().toString();
     }
 
 }
