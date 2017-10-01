decbd29ff10f8ceb1df4832d8a222f3cbc80de6c
Braian Halblaub <braianhal@gmail.com>
2017-10-01 14:01:00 -0300

    Fix setear ubicacion actual en mapa

--- app/src/main/java/com/santiago/canchaapp/app/fragment/MapClubFragment.java
+++ app/src/main/java/com/santiago/canchaapp/app/fragment/MapClubFragment.java
@@ -8,8 +8,10 @@ import android.graphics.Point;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -82,7 +84,7 @@ public class MapClubFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void getMyLocation() {
-        if (activity.checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             setMyLocation();
         } else {
             requestPermissions(
@@ -95,7 +97,7 @@ public class MapClubFragment extends Fragment implements OnMapReadyCallback {
         LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         String provider = locationManager.getBestProvider(criteria, true);
-        if (activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
             location = locationManager.getLastKnownLocation(provider);
             if (location != null) {
