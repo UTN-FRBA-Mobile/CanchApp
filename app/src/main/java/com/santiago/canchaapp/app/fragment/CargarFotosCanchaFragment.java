c38bdd0684662467e5273d0464f5181b770d4310
1251545julita <jesicarrjulita@yahoo.com.ar>
2017-10-13 02:05:20 -0300

    [Cargar_fotos_cancha] Se agrega floating button Menu. Link del fButton para confirmar una cancha. Se borra clase de ConfirmarCancha.

--- app/src/main/java/com/santiago/canchaapp/app/fragment/CargarFotosCanchaFragment.java
+++ app/src/main/java/com/santiago/canchaapp/app/fragment/CargarFotosCanchaFragment.java
@@ -15,6 +15,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -40,15 +41,18 @@ import butterknife.ButterKnife;
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static com.santiago.canchaapp.app.otros.FragmentTags.REGISTRAR_CANCHA;
+import com.github.clans.fab.FloatingActionMenu;
+import android.content.Context;
 
 public class CargarFotosCanchaFragment extends Fragment {
 
-    /*private static String APP_DIRECTORY = "MyPictureApp/";
+    private static String APP_DIRECTORY = "MyPictureApp/";
     private static String MEDIA_DIRECTORY = APP_DIRECTORY + "PictureApp";
 
     private final int MY_PERMISSIONS = 100;
     private final int PHOTO_CODE = 200;
-    private final int PHOTO_CODE_DOS = 220;
+    private final int SELECT_PICTURE = 300;
+    /*private final int PHOTO_CODE_DOS = 220;
     private final int PHOTO_CODE_TRES = 230;
     private final int PHOTO_CODE_CUATRO = 240;
     private final int PHOTO_CODE_CINCO = 250;
@@ -61,15 +65,18 @@ public class CargarFotosCanchaFragment extends Fragment {
     private final int SELECT_PICTURE_SEIS = 800;
 
     private ImageView setImage1, setImage2, setImage3, setImage4, setImage5, setImage6;
-    private ImageButton optionButton1, optionButton2, optionButton3, optionButton4, optionButton5, optionButton6;
+    private ImageButton optionButton1, optionButton2, optionButton3, optionButton4, optionButton5, optionButton6;*/
     private LinearLayout mRlView;
 
     private String mPath;
 
-    ImageView imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7;*/
+    //ImageView imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7;*/
 
-    @BindView(R.id.btnListo)
-    public Button listo;
+    FloatingActionMenu actionMenu;
+    com.github.clans.fab.FloatingActionButton fbutton1, fbutton2, fbutton3;
+
+    /*@BindView(R.id.floatingbtnListo)
+    public FloatingActionButton listo;*/
 
     public static CargarFotosCanchaFragment nuevaInstancia() {
         CargarFotosCanchaFragment fragment = new CargarFotosCanchaFragment();
@@ -87,98 +94,48 @@ public class CargarFotosCanchaFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_cargar_fotos_de_cancha, container, false);
         ButterKnife.bind(this, view);
 
-        listo.setOnClickListener(new View.OnClickListener() {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Cargar fotos de cancha");
+
+        actionMenu = (FloatingActionMenu) view.findViewById(R.id.fBtnMenu);
+        actionMenu.setClosedOnTouchOutside(false);
+
+        fbutton1 = actionMenu.findViewById(R.id.fBtnTomarFoto);
+        fbutton2 = actionMenu.findViewById(R.id.fBtnSeleccionarImagen);
+        fbutton3 = actionMenu.findViewById(R.id.fBtnGuardar);
+
+        if(mayRequestStoragePermission()){
+            fbutton1.setEnabled(true);
+            fbutton2.setEnabled(true);
+        } else{
+            fbutton1.setEnabled(false);
+            fbutton2.setEnabled(false);
+        }
+
+        fbutton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                openCamera(PHOTO_CODE);
+            }
+        });
+
+        fbutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent.createChooser(intent, "Por favor, seleccioná app de imagen"), SELECT_PICTURE);*/
+            }
+        });
+
+        fbutton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickSubMenuGuardar();
                 abrirFragmentSiguiente();
             }
         });
 
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Cargar fotos de cancha");
-
         return view;
-    }
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       setContentView(R.layout.fragment_cargar_fotos_cancha);
-
-        setImage1 = (ImageView) findViewById(R.id.btnOpciones01);
-        setImage2 = (ImageView) findViewById(R.id.btnOpciones02);
-        setImage3 = (ImageView) findViewById(R.id.btnOpciones03);
-        setImage4 = (ImageView) findViewById(R.id.btnOpciones04);
-        setImage5 = (ImageView) findViewById(R.id.btnOpciones05);
-        setImage6 = (ImageView) findViewById(R.id.btnOpciones06);
-
-        optionButton1 = (ImageButton) findViewById(R.id.btnOpciones01);
-        optionButton2 = (ImageButton) findViewById(R.id.btnOpciones02);
-        optionButton3 = (ImageButton) findViewById(R.id.btnOpciones03);
-        optionButton4 = (ImageButton) findViewById(R.id.btnOpciones04);
-        optionButton5 = (ImageButton) findViewById(R.id.btnOpciones05);
-        optionButton6 = (ImageButton) findViewById(R.id.btnOpciones06);
-        mRlView = (LinearLayout) findViewById(R.id.MyLinearLayout);
-
-        if(mayRequestStoragePermission()){
-            optionButton1.setEnabled(true);
-            optionButton2.setEnabled(true);
-            optionButton3.setEnabled(true);
-            optionButton4.setEnabled(true);
-            optionButton5.setEnabled(true);
-            optionButton6.setEnabled(true);
-        } else{
-            optionButton1.setEnabled(false);
-            optionButton2.setEnabled(false);
-            optionButton3.setEnabled(false);
-            optionButton4.setEnabled(false);
-            optionButton5.setEnabled(false);
-            optionButton6.setEnabled(false);
-        }
-
-        optionButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showOptions(SELECT_PICTURE_UNO, PHOTO_CODE);
-            }
-        });
-        optionButton2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showOptions(SELECT_PICTURE_DOS, PHOTO_CODE_DOS);
-            }
-        });
-        optionButton3.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showOptions(SELECT_PICTURE_TRES, PHOTO_CODE_TRES);
-            }
-        });
-        optionButton4.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showOptions(SELECT_PICTURE_CUATRO, PHOTO_CODE_CUATRO);
-            }
-        });
-        optionButton5.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showOptions(SELECT_PICTURE_CINCO, PHOTO_CODE_CINCO);
-            }
-        });
-        optionButton6.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                showOptions(SELECT_PICTURE_SEIS, PHOTO_CODE_SEIS);
-            }
-        });
-
-        imageView1= (ImageView) findViewById(R.id.ImageView01);
-        imageView2= (ImageView) findViewById(R.id.ImageView02);
-        imageView3= (ImageView) findViewById(R.id.ImageView03);
-        imageView4= (ImageView) findViewById(R.id.ImageView04);
-        imageView5= (ImageView) findViewById(R.id.ImageView05);
-        imageView6= (ImageView) findViewById(R.id.ImageView06);
-        imageView7= (ImageView) findViewById(R.id.ImageView07);
     }
 
     private boolean mayRequestStoragePermission() {
@@ -186,9 +143,9 @@ public class CargarFotosCanchaFragment extends Fragment {
         if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
             return true;
 
-        if((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+        /*if((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
                 (checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED))
-            return true;
+            return true;*/
 
         if((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) || (shouldShowRequestPermissionRationale(CAMERA))){
             Snackbar.make(mRlView, "@string/txtDialogExplanationPermisos1",
@@ -206,7 +163,7 @@ public class CargarFotosCanchaFragment extends Fragment {
         return false;
     }
 
-    private void showOptions(final int opcionSelect, final int opcionCamera) {
+    /*private void showOptions(final int opcionSelect, final int opcionCamera) {
         final CharSequence[] option = {"Tomar foto", "Elegir de galería", "Cancelar"};
         final AlertDialog.Builder builder = new AlertDialog.Builder(com.santiago.canchaapp.app.fragment.CargarFotosCanchaFragment.this);
         builder.setTitle("Elegí una opción");
@@ -226,7 +183,7 @@ public class CargarFotosCanchaFragment extends Fragment {
         });
 
         builder.show();
-    }
+    }*/
 
     private void openCamera(final int opcionCamera) {
         File file = new File(Environment.getExternalStorageDirectory(), MEDIA_DIRECTORY);
@@ -246,11 +203,13 @@ public class CargarFotosCanchaFragment extends Fragment {
 
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
-            startActivityForResult(intent, opcionCamera);
+            //startActivityForResult(intent, opcionCamera);
+            setLocationImage(mPath);
+            //setImageButton(mPath, setImage1);
         }
     }
 
-    @Override
+    /*@Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("file_path", mPath);
@@ -270,59 +229,19 @@ public class CargarFotosCanchaFragment extends Fragment {
         if(resultCode == RESULT_OK){
             switch (requestCode){
                 case PHOTO_CODE:
-                    setLocationImage(mPath);
-                    setImageButton(mPath, setImage1);
+                    //setLocationImage(mPath);
+                    //setImageButton(mPath, setImage1);
                     break;
-                case PHOTO_CODE_DOS:
-                    setLocationImage(mPath);
-                    setImageButton(mPath, setImage2);
-                    break;
-                case PHOTO_CODE_TRES:
-                    setLocationImage(mPath);
-                    setImageButton(mPath, setImage3);
-                    break;
-                case PHOTO_CODE_CUATRO:
-                    setLocationImage(mPath);
-                    setImageButton(mPath, setImage4);
-                    break;
-                case PHOTO_CODE_CINCO:
-                    setLocationImage(mPath);
-                    setImageButton(mPath, setImage5);
-                    break;
-                case PHOTO_CODE_SEIS:
-                    setLocationImage(mPath);
-                    setImageButton(mPath, setImage6);
-                    break;
-                case SELECT_PICTURE_UNO:
-                    Uri path1 = data.getData();
-                    setImage1.setImageURI(path1);
-                    break;
-                case SELECT_PICTURE_DOS:
-                    Uri path2 = data.getData();
-                    setImage2.setImageURI(path2);
-                    break;
-                case SELECT_PICTURE_TRES:
-                    Uri path3 = data.getData();
-                    setImage3.setImageURI(path3);
-                    break;
-                case SELECT_PICTURE_CUATRO:
-                    Uri path4 = data.getData();
-                    setImage4.setImageURI(path4);
-                    break;
-                case SELECT_PICTURE_CINCO:
-                    Uri path5 = data.getData();
-                    setImage5.setImageURI(path5);
-                    break;
-                case SELECT_PICTURE_SEIS:
-                    Uri path6 = data.getData();
-                    setImage6.setImageURI(path6);
+                case SELECT_PICTURE:
+                    //Uri path1 = data.getData();
+                    //setImage1.setImageURI(path1);
                     break;
             }
         }
-    }
+    }*/
 
     private void setLocationImage(String mPath) {
-        MediaScannerConnection.scanFile(this,
+        MediaScannerConnection.scanFile(getActivity().getApplicationContext(),
                 new String[]{mPath}, null,
                 new MediaScannerConnection.OnScanCompletedListener() {
                     @Override
@@ -344,21 +263,17 @@ public class CargarFotosCanchaFragment extends Fragment {
 
         if(requestCode == MY_PERMISSIONS){
             if(grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(com.santiago.canchaapp.app.fragment.CargarFotosCanchaFragment.this, "@string/txtTituloPermisosOk", Toast.LENGTH_SHORT).show();
-                optionButton1.setEnabled(true);
-                optionButton2.setEnabled(true);
-                optionButton3.setEnabled(true);
-                optionButton4.setEnabled(true);
-                optionButton5.setEnabled(true);
-                optionButton6.setEnabled(true);
+                Toast.makeText(getActivity().getApplicationContext(), "Permisos aceptados", Toast.LENGTH_SHORT).show();
+                fbutton1.setEnabled(true);
+                fbutton2.setEnabled(true);
             }
         }else{
-            showExplanation();
+            //showExplanation();
         }
     }
-
+/*
     private void showExplanation() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(com.santiago.canchaapp.app.fragment.CargarFotosCanchaFragment.this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity().getApplicationContext());
         builder.setTitle("@string/txtTituloPermisos");
         builder.setMessage("@string/txtDialogExplanationPermisos2");
         builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
@@ -382,11 +297,14 @@ public class CargarFotosCanchaFragment extends Fragment {
         builder.show();
     }
 */
+
+    public void clickSubMenuGuardar(){
+        Toast.makeText(getActivity().getApplicationContext(),"Tu Cancha ha sido guardada correctamente.", Toast.LENGTH_LONG).show();
+    }
+
     private void abrirFragmentSiguiente() {
-        getFragmentManager()
-            .beginTransaction()
-            .replace(R.id.club_layout, ConfirmarCancha.nuevaInstancia(), REGISTRAR_CANCHA.toString())
-            .addToBackStack(null)
-            .commit();
+        getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Club");
     }
 }
